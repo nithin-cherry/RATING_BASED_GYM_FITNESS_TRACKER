213dbb0fdfd06cffba640fc3b414f7e1f714028e
Nithin A <nithinandcherry@gmail.com>
2026-06-25 11:45:56 +0530

    app.js route updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import "./index.css";
 import Login from './pages/login';
+import Dash01 from "./pages/dash01";
+import E1 from "./pages/exe/exe01.jsx";
+import E2 from "./pages/exe/exe02";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/physique" element={<Dash01 />} />
+        <Route path="/1" element={<E1 />} />
+        <Route path="/2" element={<E2 /> } /> 
       </Routes>
     </BrowserRouter>
   );
